c00d476ce5767072cd987581c85fe548a9ad936f
mango-days <positivejasminekaur@gmail.com>
2022-01-21 23:22:48 +0530

    first commit

--- find.py
+++ find.py
@@ -1,11 +1,12 @@
-# find an element in a circularly linked list
+# 2 way linked list
 
 class Node :
     def __init__ ( self , data ) :
+        self.prev = None
         self.data = data
         self.next = None
 
-class CircularLinkedList :
+class DoubleLinkedList :
     def __init__ ( self ) :
         self.head = None
     
@@ -13,62 +14,61 @@ class CircularLinkedList :
         if self.head == None :
             self.head = Node ( data )
             self.head.next = self.head
+            self.head.prev = self.head
+            print ( self.head.prev.data ,"  ", self.head.data ,"  ", self.head.next.data )
             return
         
-        # find last element
-        temp = self.head.next
-        prev = self.head
-        flag = self.head
-        while temp != flag :
-            prev = temp
-            if temp.next != None : temp = temp.next
-            
-        # insert element
-        prev.next = Node ( data )
-        prev.next.next = flag
+        temp = self.head
+        while temp :
+            if temp.next != self.head : temp = temp.next
+            else : break
+        
+        temp.next = Node ( data )
+        temp.next.next = self.head
+        temp.next.prev = temp
+        
+        self.head.prev = temp.next
+        print ( temp.next.prev.data ,"  ", temp.next.data ,"  ", temp.next.next.data )
         return
     
     def printList ( self ) :
-        if self.head == None :
-            print ( "List empty" )
-            return
+        if self.head == None : return
         
+        print ( "pre", "node" , "next" )
+
+        print ( self.head.prev.data ,"  ", self.head.data ,"  ", self.head.next.data )
         temp = self.head.next
         prev = self.head
         flag = self.head
         while temp != flag :
-            print ( temp.data )
+            print ( temp.prev.data ,"  ", temp.data ,"  ", temp.next.data )
             temp = temp.next
         return
     
     def find ( self , data ) :
-        if self.head == None :
-            print ( "Not found" )
-            return
+        if self.head == None : return
         
-        temp = self.head.next
-        found = False
-        flag = self.head
-        count = 0
-        
-        while temp != flag:
-            count += 1
-            if temp.data == data :
-                found = True
-                break
-            temp = temp.next
-            
-        if found == True : print ( "Found at :" , count )
-        else : print ( "Not found" )
-        
-        return
-            
-CllObj = CircularLinkedList ()
-
-for index in range ( 10 ) : 
-    CllObj.insert ( index )
+        index = 1
+        temp = self.head
+        while temp :
+            if temp.data == data : return index
+            elif temp.next != self.head : temp = temp.next
+            else : return None
+            index += 1
     
-CllObj.find (11)
-CllObj.find (6)
-CllObj.find (2)
-CllObj.find (0)
+
+Obj = DoubleLinkedList ()
+
+print ( "inserting nodes ---" )
+print ( "pre", "node" , "next" )
+for index in range (9) : Obj.insert (index+1)
+
+print ("")
+print ( "nodes inserted as ---" )
+Obj.printList()
+
+print ("")
+print ( "node with data 5 is found at index:" , Obj.find(5) )
+print ( "node with data 10 is found at index:" , Obj.find(10) )
+print ( "node with data 1 is found at index:" , Obj.find(1) )
+print ( "node with data 9 is found at index:" , Obj.find(9) )
